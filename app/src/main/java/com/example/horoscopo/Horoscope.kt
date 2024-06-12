6cf0b093757c96b7f38394543cbf987f6ebd72d6
YO <ojara@gmx.com>
2024-06-12 09:44:38 +0200

    Arreglados los errores del proyecto

---
+++ app/src/main/java/com/example/horoscopo/Horoscope.kt
@@ -0,0 +1,5 @@
+package com.example.horoscopo
+
+class Horoscope(val id:String, val name:Int, val description: Int, val logo:Int) {
+
+}
--- app/src/main/java/com/example/horoscopo/Horoscopo.kt
+++
@@ -1,5 +0,0 @@
-package com.example.horoscopo
-
-class Horoscope(val id:String, val name:String, val logo:Int) {
-
-}
