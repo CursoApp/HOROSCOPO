f2f9c37273c54d4c1c6bab681f261ac21d9c9c44
YO <ojara@gmx.com>
2024-06-13 13:30:59 +0200

    Horoscopo corregido y con colores del jueves 11 de junio

--- app/src/main/java/com/example/horoscopo/Horoscope.kt
+++ app/src/main/java/com/example/horoscopo/Horoscope.kt
@@ -1,3 +1,5 @@
 package com.example.horoscopo
 
-class Horoscope(val id:String, val name:Int, val description:Int, val logo:Int)
+import android.graphics.Color
+
+class Horoscope(val id:String, val name:Int, val description:Int, val logo:Int, val color: Int)
